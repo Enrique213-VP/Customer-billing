6c2305431b8edeba7b25c410300856fbde153015
Sergio <colombia00028@gmail.com>
2021-07-18 22:45:31 -0500

    Interfaz desde la main

---
+++ src/facturaproductos/FacturaProductos.java
@@ -0,0 +1,14 @@
+package facturaproductos;
+
+public class FacturaProductos {
+
+    public static void main(String[] args) {
+        
+        InterfaceFactura disable = new InterfaceFactura();
+        
+       disable.setLocationRelativeTo(null);
+       disable.setTitle("Interfaz Gráfica del Applet");
+       disable.setVisible(true);
+    }
+    
+}
